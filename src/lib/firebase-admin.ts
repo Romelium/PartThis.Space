6ca1d4915aa59b3cbd532743d8f55ac1c9a34ce6
Romelianism <Romelianism@gmail.com>
2022-03-27 00:35:06 +0800

    feat: add firebase-admin
    Needed to authenticate users in the backend.

---
+++ src/lib/firebase-admin.ts
@@ -0,0 +1,9 @@
+import * as admin from "firebase-admin";
+
+export const app = !admin.apps.length
+  ? admin.initializeApp({
+      credential: admin.credential.cert(
+        JSON.parse(process.env.GOOGLE_APPLICATION_CREDENTIALS!)
+      ),
+    })
+  : admin.app();
